ae867885cb05d1e97c35d968d6d8a57e720246b1
Pavel <145332531+pavel-truely@users.noreply.github.com>
2024-03-13 21:08:44 +0100

    Update update_linear_issues.sh

--- .github/scripts/update_linear_issues.sh
+++ .github/scripts/update_linear_issues.sh
@@ -8,6 +8,8 @@ commits=$(git log release/dev -100 --pretty=format:'%H %s')
 # Find 20 commits matching the pattern and extract issue ids
 issue_ids=$(echo "$commits" | grep -E "^.*Merge pull request.*tes-[0-9]+-.*$" | sed -E 's/.*tes-([0-9]+)-.*/TES-\1/' | tr '[:lower:]' '[:upper:]' | head -20)
 
+echo $issue_ids
+
 # Initialize an empty array to track processed issue IDs
 processed_issues=""
 
